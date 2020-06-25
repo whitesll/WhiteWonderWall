a4d8d647902ca1753488037043b1fbafc28874fa
whitesllgit <t-nisioka@hotmail.com>
2020-06-25 11:09:20 +0900

    環境設定

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,15 @@ Bundler.require(*Rails.groups)
 module WhiteWonderWall
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
     config.load_defaults 5.2
-
+    config.generators.template_engine = :slim
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
