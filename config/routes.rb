2060aa580ab779edf064b7c0cf7feae316c17e9d
whitesllgit <t-nisioka@hotmail.com>
2020-08-06 23:01:02 +0900

    about作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "videos#index"
-  resources :videos, only: [:show]
+  resources :videos, only: [:show] do
+    get :about, on: :collection
+  end
 end
