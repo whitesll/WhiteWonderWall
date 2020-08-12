c8b96566ce24a7218ccdc6bef0dda466e3eca91f
whitesllgit <t-nisioka@hotmail.com>
2020-08-13 00:29:19 +0900

    ユーザー編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
-  private
-  def after_update_path_for(resource)
-    editpage_users_path
-  end
-
-  def after_sign_up_path_for(resource)
-    createpage_users_path
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    confirmations: "users/confirmations",
+    sessions: "users/sessions",
+    passwords: "users/passwords"
+  }
   root "videos#index"
   resources :videos, only: [:show] do
     get :about, on: :collection
+  end
+  resources :users, only: [:show, :edit, :update] do
+    collection do
+      get :createpage
+      get :editpage
+    end
   end
 end
