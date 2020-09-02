54bf68ee10d915d1796c57bdc0388e28cccb0ef7
whitesllgit <t-nisioka@hotmail.com>
2020-09-02 20:44:59 +0900

    動画一覧作成

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   has_many :posts
+  has_many :videos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments
+  has_many :videocomments
   has_many :likes, dependent: :destroy
   has_many :like_comments, through: :likes, source: :comment
+  has_many :videolikes, dependent: :destroy
+  has_many :videolike_comments, through: :videolikes, source: :videocomment
   
   validates :nickname, presence: true, length: { maximum: 6 }
 end
