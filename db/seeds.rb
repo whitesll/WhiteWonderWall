36314b28afeb3142e6f4440ecd6e3bd5da8b2835
whitesllgit <t-nisioka@hotmail.com>
2020-09-17 16:50:07 +0900

    デモデータ video追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,13 +69,140 @@ Post.create(
 
 Video.create(
   [
-
     { game_id: "1", 
       title: "【CIV5】15分で分かるＣＩＶ５", 
       url: "30Iybz7-ffg", 
       date: "2017-02-23", 
-      description: "１５分シリーズの第一回目はあの人気ゲームシドマイヤーズシヴィライゼーション。全５話でお送りします", 
+      description: "１５分シリーズの第一回目はあの人気ゲームシドマイヤーズシヴィライゼーション。
+      全５話でお送りします", 
       thumbnail: "https://i.ytimg.com/vi/30Iybz7-ffg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCZyS5388KmBhG0fXKdyIHqdBU4cg"
+    },
+    { game_id: "5", 
+      title: "[AoE2 : 初心者向け解説付]vs ResonanceBot 5-1c Very Hard with Mouse only[ダイジェスト版]", 
+      url: "ybLfMf8DA48", 
+      date: "2018-12-09", 
+      description: "Steamワークショップにて上位人気のカスタムAI「ResonanceBot 5-1c」を、 
+      難易度「非常に難しい」に設定し、それをマウス操作だけで攻略します。 
+      攻略ついでに、AoE2の対戦で根幹を成す所をアピールするのが目的となります。", 
+      thumbnail: "https://i.ytimg.com/vi/fL_uoF3w9UU/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCMzzi7-aC8VOEfGX09XPvsmCNFdA"
+    },
+    { game_id: "2", 
+      title: "【WoWS】見るだけでWoWSをプレイしたくなるような短編集 　#1【ゆっくり実況/茶番】", 
+      url: "8YKLu9YkEHw", 
+      date: "2020-06-11", 
+      description: "WoWSのゆっくり実況/茶番です。
+      今回は皆様から受け取ったリプレイ集になります。
+      普段の実況に比べて撮影する時間や茶番を考えて作る時間が長くなるので不定期でやっていく予定", 
+      thumbnail: "https://i.ytimg.com/vi/8YKLu9YkEHw/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLC7mNt6DYx_oYgYvgAe-JUMFW4Zwg"
+    },
+    { game_id: "2", 
+      title: "【WoWs】強すぎて販売停止になった駆逐艦!?　優秀な性能を持つ神風駆逐艦出撃！　　ゆっくりの海戦７４【ゆっくり実況】", 
+      url: "BNLJWAqwBy4", 
+      date: "2020-07-05", 
+      description: "WoWSのゆっくり実況になります！
+      今回は神風Rに乗っていきます。
+      とりあえずこれ乗っとけばハズレはない。", 
+      thumbnail: "https://i.ytimg.com/vi/BNLJWAqwBy4/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAJKI5EPpVMoz1sqGiJEPBSeQwvGg"
+    },
+    { game_id: "2", 
+      title: "【WoWS雑談】WoWSはクソゲー！？　レビューを見ながら雑談してみた【ゆっくり解説】", 
+      url: "u-g45mdzOy4", 
+      date: "2018-12-21", 
+      description: "WoWSについてのゆっくり雑談動画です。
+      今回はとあるオンラインゲームレビューのサイトを偶然見つけたので、
+      WoWSの評価、レビューを見ての雑談動画になります。", 
+      thumbnail: "https://i.ytimg.com/vi/u-g45mdzOy4/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBX2QRg2zeywzr35z8beYf9QNQOuw"
+    },
+    { game_id: "3", 
+      title: "【WoT】World of Tanks初心者教育用動画", 
+      url: "YFdFYpGOYTY", 
+      date: "2018-07-07", 
+      description: "00：00～03：00　クライアントの設定
+      03：00～06：00　ガレージでの準備
+      06：00～08：45　基本的な立ち回り
+      08：45～12：20　豚飯の角度について
+      12：20～14：20　Tanks.ggの使い方
+      14：20～17：45　ハルダウンのやり方
+      17：45～21：55　茂みの使い方
+      21：55～24：30　15mルールのやり方
+      24：30～26：30　NDK対応のやり方", 
+      thumbnail: "https://i.ytimg.com/vi/YFdFYpGOYTY/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBeuJwBHcRtD1vYEy3xSZRLqIoOQw"
+    },
+    { game_id: "3", 
+      title: "【WoT】初心を思い出す為に戦車道を学ぶ【ゆっくり実況(草)1】", 
+      url: "Sil04Fbytdk", 
+      date: "2014-10-23", 
+      description: "最近勝てる勝負以外めっきりしなくなったというか、勝ちたいが先行するというか。
+      心が狭くなった気がするので、完全アウェーのWoTで初心者の気持ちを思い出します。
+      
+      完全素人、操作が出来るレベルだと思って下さい、戦車の知識は皆無です。
+      初心者なりに、Noobなりに色々考察してからプレイした結果がこれなんですよね。(どうやったら勝てるんだこれ)
+      ネタの提供・要望・誹謗中傷・ご意見などはコメントへ！", 
+      thumbnail: "https://i.ytimg.com/vi/Sil04Fbytdk/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAc7wk_6_XhUlX-QHMJADWmT0_GJA"
+    },
+    { game_id: "3", 
+      title: "【ゆっくり実況】WOT初心者は横転しても動じない【World of Tanks】", 
+      url: "jKCK7yaPZJY", 
+      date: "2020-02-28", 
+      description: "https://redir.wargaming.net/9c1we1is/...
+      ▲ゲームダウンロードはコチラ　（無料でプレイ）
+      
+      提供：World of Tanks　ワールドオブタンクス
+      
+      『World of Tanks』とは
+      戦闘車輌に特化した、世界で最初の、かつ唯一の、チームベース・マルチプレイヤー・オンラインゲームです。
+      世界中のプレイヤーと共に、この壮大な陸戦が展開されるオンラインのバトルフィールドへ身を投じましょう。
+      アメリカ、ドイツ、ソビエト連邦、フランス、イギリス、スウェーデン、チェコスロバキア、中国、イタリア、ポーランド、
+      そして日本から総計550以上の車輛が登場します！
+      
+      【酒桜コメント】
+      またまたwotの案件のご相談をいただきました。
+      誠にありがとうございます！
+      決して横転してそのままどうにもできなくなるという
+      よくわからないプレイを見せないよう、頑張ります！！！！！", 
+      thumbnail: "https://i.ytimg.com/vi/jKCK7yaPZJY/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDY13l3HW_OugavLTbJU1Cjfa_JyA"
+    },
+    { game_id: "4", 
+      title: "[東方某酒宴] 尻 & 主催 vs ななし & まげ", 
+      url: "g_cH-tVNCCc", 
+      date: "2014-04-09", 
+      description: "東方蟒酒宴の対戦。
+      わいわい言いながらやってる系。", 
+      thumbnail: "https://i.ytimg.com/vi/g_cH-tVNCCc/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDoNMDTVXtiba4YWYAmszXk2e32PQ"
+    },
+    { game_id: "1", 
+      title: "【Civ5 BNW】創造主でアレクサンドリア図書館建設", 
+      url: "6RSOP8B_CZs", 
+      date: "2017-07-24", 
+      description: "以前アップしたものにゆっくり＆字幕を付けたものです。
+      これぐらいの長さの動画ならサクっと編集できるんですけどね～", 
+      thumbnail: "https://i.ytimg.com/vi/6RSOP8B_CZs/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDdkct992eqAi392RdN2oZ8dbZ5Mw"
+    },
+    { game_id: "1", 
+      title: "【Civ5 BNW】創造主科学勝利ターンアタック229Tクリア動画", 
+      url: "bXGYEKR_OTI", 
+      date: "2017-07-24", 
+      description: "Civ5の創造主科学勝利ターンアタック動画です。
+      今回はゆっくり実況と字幕を付けてみました。
+      【追記】
+      BGMが著作権に引っかかった対応で、該当箇所の1h39m～3分ほど無音になります。ご了承ください。", 
+      thumbnail: "https://i.ytimg.com/vi/bXGYEKR_OTI/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDipB4vwhtUoMyZatThX5cWfh-Nww"
+    },
+    { game_id: "1", 
+      title: "Civilization5初心者向け解説動画【始めたい、始めたばかりの方向け】", 
+      url: "EpmKy6j7wGo", 
+      date: "2015-04-04", 
+      description: "今回の動画は大好きなciv5の普及のため、
+      始めようとしている方へ向けの導入支援動画を作ってみました！
+      civは面白いのに、とっつきにくいイメージが先行しがちなので、
+      そのとっつきにくさを払拭できればと思い作ってみました。
+      ※中・上級の方は是非ご支援お願いします！補足説明や質問への回答等お力をおかしください！
+      あくまで普及を目的としているので、難しい戦術説明ではなく初心者支援という目線でお願いします
+      尚、本ゲームはアマゾンで購入可能なので市場に参考までに置いておきます。
+      ダウンロード版はインストール、プレイするには「Steam」というソフトのインストールが先に必要になります
+      （下記URL参照）
+      http://store.steampowered.com/?l=japa...", 
+      thumbnail: "https://i.ytimg.com/vi/EpmKy6j7wGo/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBySZI_THe3xzr0FW1LGpdEC7fe5g"
     }
   ]
 )
