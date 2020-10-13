fba49bf166128788a164fa49ca89a3b0409f2da7
whitesllgit <t-nisioka@hotmail.com>
2020-10-13 22:10:38 +0900

    違反解消

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    sequence(:email) {Faker::Internet.email}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    nickname              { "abe" }
+    sequence(:email) { Faker::Internet.email }
+    password              { "00000000" }
+    password_confirmation { "00000000" }
   end
 
 end
